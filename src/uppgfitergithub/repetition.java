74bc95e719df7405e73929865fbcb8cce94735bd
Valdaserides <max.valdaseridesolofsson.te16c@gmail.com>
2018-09-05 12:22:28 +0200

    uppgift1

--- src/uppgfitergithub/repetition.java
+++ src/uppgfitergithub/repetition.java
@@ -1,5 +1,18 @@
 package uppgfitergithub;
 
 public class repetition {
-
+	
+	public static void main(String[] args) {
+		
+		int sum = 0;
+		int[] arr = {1,2,3,4,3,2};
+		
+		for (int i = 0; i < arr.length; i++) {
+			
+			sum = sum + arr[i];
+			
+		}
+		System.out.println(sum);
+	}
+	
 }
