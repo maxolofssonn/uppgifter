8e6fc5897f395e0b0ba8cb264c93207c8f4df4f1
Valdaserides <max.valdaseridesolofsson.te16c@gmail.com>
2018-09-05 12:59:11 +0200

    uppgift5

---
+++ src/uppgfitergithub/Person.java
@@ -0,0 +1,15 @@
+package uppgfitergithub;
+
+public class Person {
+
+	private int age;
+	private String name;
+	
+	public Person(int age, String name){
+		
+		this.age = age;
+		this.name = name;
+		
+	}
+	
+}
